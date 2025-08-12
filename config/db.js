6e1244cf901f46d76b24360ac49e5de4447ec3f1
Subesh Sarkar <subesh.sarkar1980@gmail.com>
2025-08-12 13:20:19 +0530

    Created MongoDB Schemas

---
+++ config/db.js
@@ -0,0 +1,71 @@
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema;
+const Objectid = Schema.ObjectId;
+mongoose.connect('process.env.MONGO_URL')
+
+const users = new Schema({
+    firstName : String,
+    lastName : String,
+    email : String,
+    password : String,
+    token : String
+})
+
+const admins = new Schema({
+    firstName : String,
+    lastName : String,
+    email : String,
+    password : String,
+    token : String
+})
+
+const carts = new Schema({
+    userID : Objectid,
+    items : [{
+        productID : Objectid,
+        quantity : Number
+    }],
+    lastUpdated : {
+        type : Date,
+        default : Date.now
+    }
+})
+
+const products = new Schema({
+    name : String,
+    description : String,
+    price : Number,
+    category : String,
+    stock : Number,
+    imgUrl : String,
+    createdAt : Date.now,
+    lastUpdates : {
+        type : Date,
+        default : Date.now,
+    }
+})
+
+const orders = new Schema({
+    userID : Objectid,
+    items : [{
+        productID : Objectid,
+        quantity : Number,
+        price : Number
+    }],
+    totalAmount : Number,
+    purchaseDate : Date.now
+})
+
+const userModel = mongoose.model('users', users)
+const adminModel = mongoose.model('admins', admins)
+const productModel = mongoose.model('products', products)
+const cartsModel = mongoose.model('carts', carts)
+const orderModel = mongoose.model('orders', orders)
+
+module.exports = {
+    userModel,
+    adminModel,
+    productModel,
+    cartsModel,
+    orderModel
+}
